fc1af9a37409e6df0ed718e83842902d1d4cf2cc
gycsaba <gyuris.csaba@vasvari.org>
2023-11-13 19:33:04 +0100

    TeacherRepo

---
+++ Kreata.Backend/Repos/TeacherRepo.cs
@@ -0,0 +1,25 @@
+﻿using Kreata.Backend.Context;
+using Kreata.Backend.Datas.Entities;
+
+namespace Kreata.Backend.Repos
+{
+    public class TeacherRepo : ITeacherRepo
+    {
+        private readonly KretaContext _dbContext;
+
+        public TeacherRepo(KretaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Teacher> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Teacher? GetBy(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
