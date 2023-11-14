09c6c7d3cc806967eb03502e57903bbbef364603
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-11-14 08:25:00 +0100

    Teacher repo

---
+++ Kreata.Backend/Repos/TeacherRepo .cs
@@ -0,0 +1,25 @@
+﻿using Kreata.Backend.Context;
+using Kreata.Backend.Datas.Entities;
+
+namespace Kreata.Backend.Repos
+{
+    public class TeacherRepo : ITeacherRepo
+    {
+        private readonly KretaContext _dbContext;
+
+        public TeacherRepo(KretaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Teacher> GetAll()
+        {
+            return _dbContext.Teachers.ToList();
+        }
+
+        public Teacher? GetBy(Guid id)
+        {
+            return _dbContext.Teachers.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
